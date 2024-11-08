f6e14732b091cec669eb390bcfc89fc6ae224497
Mandy Urban <mandy.urban@visualvest.de>
2024-11-08 19:05:53 +0100

    add detail page and styles

--- src/app/advent-calendar-details/advent-calendar-details.component.ts
+++ src/app/advent-calendar-details/advent-calendar-details.component.ts
@@ -1,14 +1,27 @@
-import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { AdventDoor, adventDoors } from '../data/calendar';
 
 @Component({
-  selector: 'app-advent-calendar-details',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl: './advent-calendar-details.component.html',
-  styleUrl: './advent-calendar-details.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-advent-calendar-details',
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    templateUrl: './advent-calendar-details.component.html',
+    styleUrl: './advent-calendar-details.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdventCalendarDetailsComponent { }
+export class AdventCalendarDetailsComponent implements OnInit {
+    door!: AdventDoor;
+
+    constructor(private readonly route: ActivatedRoute) {}
+
+    ngOnInit(): void {
+        const id = this.route.snapshot.params['uuid'];
+        this.door = this.findDayById(id);
+    }
+
+    findDayById(id: string): AdventDoor {
+        return adventDoors.find((x) => x.uuid === id)!;
+    }
+}
--- src/app/advent-calendar-door/advent-calendar-door.component.ts
+++ src/app/advent-calendar-door/advent-calendar-door.component.ts
@@ -3,32 +3,32 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { AdventDoor } from '../data/calendar';
 import { Router } from '@angular/router';
 
-
 @Component({
-  selector: 'app-advent-calendar-door',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './advent-calendar-door.component.html',
-  styleUrl: './advent-calendar-door.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-advent-calendar-door',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './advent-calendar-door.component.html',
+    styleUrl: './advent-calendar-door.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AdventCalendarDoorComponent {
+    constructor(private router: Router) {}
 
-  constructor(
-    private router: Router
-  ) {}
+    currentDay = new Date().getDate(); // aktuelles Tagesdatum
+    @Input({ required: true }) door!: AdventDoor;
 
-  currentDay = new Date().getDate(); // aktuelles Tagesdatum
-  @Input({ required: true }) door!: AdventDoor;
-
-  openDoor(door: AdventDoor) {
-    if (this.currentDay >= door.day) {
-      door.isOpen = !door.isOpen; // Set door as open
-        setTimeout(() => {
-        this.router.navigate(['/door', door.uuid]); // Weiterleitung zur Detailseite
-      }, 1000); // Nach der Animation (1 Sekunde Verzögerung)
-    } else {
-      alert('You can’t open this door yet!');
+    openDoor(door: AdventDoor) {
+        if (this.currentDay >= door.day) {
+            door.isOpen = !door.isOpen; // Set door as open
+            setTimeout(() => {
+                this.navigate(door.uuid);
+            }, 1000); // Nach der Animation (1 Sekunde Verzögerung)
+        } else {
+            alert('You can’t open this door yet!');
+        }
     }
-  }
+
+    navigate(id: string): void {
+        this.router.navigate(['/door', id]); // Weiterleitung zur Detailseite
+    }
 }
